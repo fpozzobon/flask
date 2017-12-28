b7c661789e2a060302847db8d8c0b7064948f8b1
fabienPozzobon <fabien.pozzobon@gmail.com>
2017-12-28 15:07:23 +0000

    UPDATE : removed unit test redundancy

--- tests/controllers/test_main.py
+++ tests/controllers/test_main.py
@@ -4,23 +4,7 @@
 import pytest
 from unittest.mock import MagicMock
 from flask import json
-
-def createSong(id):
-  return {
-          '_id': id,
-          'artist': "artist%s"%id,
-          'title': "title%s"%id,
-          'level': "level%s"%id,
-          'difficulty': "difficulty%s"%id,
-          'released': "released%s"%id,
-          'ratings': []
-        }
-
-def createNSongs(n):
-  result = []
-  for i in range(n):
-    result.append(createSong(str(i)))
-  return result
+from tests.utils import createSong, createNSongs
 
 def mockSongService(app):
   with app.app_context():
